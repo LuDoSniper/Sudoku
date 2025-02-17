6348264f4068bdf95db3e9e0dbbc958821d0fc14
Mathys DC <rokassd9@gmail.com>
2025-02-17 12:51:42 +0100

    Add backtracking recursif for Guigui

---
+++ solvers/backtracking_recursif.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+def find_next_empty(grid, size):
+    """ Trouve la prochaine cellule vide (0) dans la grille. """
+
+def is_valid(grid, num, row, col, square_size):
+    """ Vérifie si un numéro peut être placé dans la cellule donnée. """
+
+def backtracking_recursif(grid):
+    """ Solveur de Sudoku utilisant le backtracking récursif. """
+    size = grid.size
+    square_size = int(sqrt(size))
+
+    # Trouver la prochaine cellule vide
+    cell = ...
+    if not cell:
+        ...
+
+    ...
+
+    for ... in range(...):
+
+
+    return ...
