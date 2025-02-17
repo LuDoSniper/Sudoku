340f0260c40ec41d4ab1ed041dc5a10ad331158e
TanguyDLT <tanguy.de-la-taille@hesias.fr>
2025-02-17 14:50:50 +0100

    backtracking recursif

--- tests/test_time_solvers.py
+++ tests/test_time_solvers.py
@@ -3,14 +3,16 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from solvers.backtracking_iteratif_pile import backtracking_iteratif_pile
+from solvers.backtracking_recursif import backtracking_recursif
 from solvers.heuristic_method import heuristic_method
 from tools.get_time import get_time
 from tools.generator import generate_full, calibrate
 from models.Grid import Grid
 
-grid = Grid(25)
+grid = Grid(16)
 generate_full(grid)
-calibrate(grid, "easy")
+calibrate(grid, "hard")
 
 print(get_time(backtracking_iteratif_pile, grid))
+print(get_time(backtracking_recursif, grid))
 print(get_time(heuristic_method, grid))
